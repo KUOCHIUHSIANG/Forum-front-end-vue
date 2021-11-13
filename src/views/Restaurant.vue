32ce3fde6680c8e423192f2836523c385b3dce72
Jater <p833795p@gmail.com>
2021-11-13 22:27:18 +0800

    feat: build CreateComment component and change <a> to <router-link>

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -6,12 +6,14 @@
     <!-- 餐廳評論 RestaurantComments -->
     <RestaurantComments :restaurant-comments="restaurantComments" @after-delete-comment="afterDeleteComment" />
     <!-- 新增評論 CreateComment -->
+    <CreateComment :restaurant-id="restaurant.id" @after-create-comment="afterCreateComment" />
   </div>
 </template>
 
 <script>
 import RestaurantDetail from '../components/RestaurantDetail.vue'
 import RestaurantComments from '../components/RestaurantComments.vue'
+import CreateComment from '../components/CreateComment.vue'
 
 const dummyData = {
   restaurant: {
@@ -99,10 +101,22 @@ const dummyData = {
   isLiked: false,
 };
 
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: '管理者',
+    email: 'root@example.com',
+    image: 'https://i.pravatar.cc/300',
+    isAdmin: true
+  },
+  isAuthenticated: true
+}
+
 export default {
   components: {
     RestaurantDetail,
-    RestaurantComments
+    RestaurantComments,
+    CreateComment
   },
   data() {
     return {
@@ -118,6 +132,7 @@ export default {
         isFavorited: false,
         isLiked: false
       },
+      currentUser: dummyUser.currentUser,
       restaurantComments: []
     }
   },
@@ -143,6 +158,20 @@ export default {
     afterDeleteComment(commentId) {
       // 以 filter 保留未被選擇的 comment.id
       this.restaurantComments = this.restaurantComments.filter(comment => comment.id !== commentId)
+    },
+    afterCreateComment (payload) {
+      const { commentId, restaurantId, text } = payload
+      this.restaurantComments.push({
+        id: commentId,
+        RestaurantId: restaurantId,
+        User: {
+          id: this.currentUser.id,
+          name: this.currentUser.name
+        },
+        text,
+        createdAt: new Date()
+      })
+      console.log(this.restaurantComments)
     }
   },
   created() {
