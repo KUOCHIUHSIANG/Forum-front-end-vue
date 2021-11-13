8c2440d8c66609be168450ee3f2934c4c1fa94a3
Jater <p833795p@gmail.com>
2021-11-13 14:07:55 +0800

    feat: build UsersTop component

--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -1,17 +1,111 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">
-      美食達人
-    </h1>
+    <h1 class="mt-5">美食達人</h1>
+    <hr />
+    <div class="row text-center">
+      <div v-for="user in users" :key="user.id" class="col-3">
+        <a href="#">
+          <img
+            :src="user.image | checkImage"
+            width="140px"
+            height="140px"
+          />
+        </a>
+        <h2>{{ user.name }}</h2>
+        <span class="badge badge-secondary">追蹤人數：{{ user.FollowerCount }}</span>
+        <p class="mt-3">
+          <button v-if="user.isFollowed" @click.stop.prevent="deleteFollow(user)" type="button" class="btn btn-danger">取消追蹤</button>
+          <button v-else @click.stop.prevent="addFollow(user)" type="button" class="btn btn-primary">追蹤</button>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import NavTabs from '../components/NavTabs.vue'
+import NavTabs from "../components/NavTabs.vue";
+
+const dummyData = {
+  users: [
+    {
+      id: 1,
+      name: "root",
+      email: "root@example.com",
+      password: "$2a$10$vEMn/13hstMM3LFiHVlHrelX0iYsn1/IBX8nDgftLcvoocj9h7rfq",
+      isAdmin: true,
+      image: 'https://wondrousss.info/quotes/files/avatar_img_668.jpg',
+      createdAt: "2021-11-10T08:18:00.000Z",
+      updatedAt: "2021-11-10T08:18:00.000Z",
+      Followers: [],
+      FollowerCount: 0,
+      isFollowed: false,
+    },
+    {
+      id: 2,
+      name: "user1",
+      email: "user1@example.com",
+      password: "$2a$10$F/51ajI/72crcWFNCTLdleAlm51gnrBmTYkxsRhDmh2AVw7TaiXxK",
+      isAdmin: false,
+      image: 'https://i.pinimg.com/originals/f1/c1/32/f1c13292460e637545773a0f481a7381.jpg',
+      createdAt: "2021-11-10T08:18:00.000Z",
+      updatedAt: "2021-11-10T08:18:00.000Z",
+      Followers: [],
+      FollowerCount: 0,
+      isFollowed: false,
+    },
+    {
+      id: 3,
+      name: "user2",
+      email: "user2@example.com",
+      password: "$2a$10$qFlU7osqmyJPIF0tMN9Dq.gLt3CoPYVJod71ch1EmSkzk4zSZI53i",
+      isAdmin: false,
+      image: null,
+      createdAt: "2021-11-10T08:18:00.000Z",
+      updatedAt: "2021-11-10T08:18:00.000Z",
+      Followers: [],
+      FollowerCount: 0,
+      isFollowed: false,
+    },
+  ],
+};
+
 export default {
   components: {
-    NavTabs
+    NavTabs,
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    fetchTopUsers() {
+      this.users = {
+        ...dummyData.users,
+      }
+    },
+    addFollow(user) {
+      user.isFollowed = true
+      console.log(dummyData.users[0].isFollowed)  // true
+      console.log(this.users[0].isFollowed)  // true
+    },
+    deleteFollow(user) {
+      user.isFollowed = false
+    }
+  },
+  filters: {
+    checkImage(image) {
+      if(!image) {
+        return 'https://media.istockphoto.com/vectors/anonymous-vector-icon-incognito-sign-privacy-concept-human-head-with-vector-id1284693553?b=1&k=20&m=1284693553&s=170667a&w=0&h=kuHXRL9c6sXDM--AgaQaW-4w1KFj3aEh_Xl4LvJHj8s='
+      }else {
+        return image
+      }
+    }
+  },
+  created(){
+    this.fetchTopUsers()
+    console.log(dummyData.users[0].isFollowed)
   }
-}
+};
 </script>
