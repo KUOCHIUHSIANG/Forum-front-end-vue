0628e41874de538b336a4592cea3cdb491658cb7
Jater <p833795p@gmail.com>
2021-11-14 22:39:16 +0800

    feat: build User.vue view

---
+++ src/views/User.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="container">
+    <!-- UserProfileCard -->
+    <div class="row">
+      <div class="col-md-4">
+        <!-- UserFollowingsCard -->
+        <br />
+        <!-- UserFollowersCard -->
+      </div>
+      <div class="col-mb-8">
+        <!-- UserCommentsCard -->
+        <br />
+        <!-- UserFavoritedRestaurantsCard -->
+      </div>
+    </div>
+  </div>
+</template>
