2706a438af1ec8cf04490c5ac9c08716c02d2e66
Jater <p833795p@gmail.com>
2021-11-17 17:29:43 +0800

    feat: build UserFavoritedRestaurantsCard component

---
+++ src/components/UserFavoritedRestaurantsCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card-header"><strong> {{ favoritedRestaurants.length }} </strong> 收藏的餐廳</div>
+    <div class="card-body">
+      <router-link :to="{name: 'restaurant', params: { id: favoritedRestaurant.id }}" v-for="favoritedRestaurant in favoritedRestaurants" :key="favoritedRestaurant.id">
+        <img
+          :src="favoritedRestaurant.image"
+          width="60"
+          height="60"
+          class="avatar mr-1"
+        />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    favoritedRestaurants: {
+      type: Array
+    }
+  }
+}
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -2,7 +2,7 @@
 <div class="album py-5 bg-light">
   <div class="container">
     <!-- UserProfileCard -->
-    <UserProfileCard :initial-profile="profile" :initial-isFollowed="isFollowed"/>
+    <UserProfileCard :initial-profile="profile" :initial-isFollowed="isFollowed" />
     <div class="row">
       <div class="col-md-4">
         <!-- UserFollowingsCard -->
@@ -16,6 +16,7 @@
         <UserCommentsCard :comments="profile.Comments" />
         <br />
         <!-- UserFavoritedRestaurantsCard -->
+        <UserFavoritedRestaurantsCard :favoritedRestaurants="profile.FavoritedRestaurants" />
       </div>
     </div>
   </div>
@@ -27,6 +28,7 @@ import UserProfileCard from '../components/UserProfileCard.vue'
 import UserFollowingsCard from '../components/UserFollowingsCard.vue'
 import UserFollowersCard from '../components/UserFollowersCard.vue'
 import UserCommentsCard from '../components/UserCommentsCard.vue'
+import UserFavoritedRestaurantsCard from '../components/UserFavoritedRestaurantsCard.vue'
 
 const dummyData = {
   profile: {
@@ -1291,7 +1293,8 @@ export default {
     UserProfileCard,
     UserFollowingsCard,
     UserFollowersCard,
-    UserCommentsCard
+    UserCommentsCard,
+    UserFavoritedRestaurantsCard
   },
   data() {
     return {
