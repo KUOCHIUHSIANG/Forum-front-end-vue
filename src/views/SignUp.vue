3e865cd3ca42c2fd659fe2ea8a5d7500fe6ec19e
Jater <p833795p@gmail.com>
2022-05-01 01:51:33 +0800

    feat: finish SignUp API

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -70,8 +70,10 @@
       <button
         class="btn btn-lg btn-primary btn-block mb-3"
         type="submit"
+        :disabled="isProcessing"
       >
-        Submit
+        {{ isProcessing ? '處理中...' : 'Submit'}}
+        
       </button>
 
       <div class="text-center mb-3">
@@ -90,26 +92,80 @@
 </template>
 
 <script>
+import authorizationAPI from './../apis/authorization'
+import { Toast } from './../utils/helpers'
+
 export default {
   data () {
     return {
       name: '',
       email: '',
       password: '',
-      passwordCheck: ''
+      passwordCheck: '',
+      isProcessing: false
     }
   },
   methods: {
-    handleSubmit () {
-      const data = JSON.stringify({
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        passwordCheck: this.passwordCheck
-      })
+    async handleSubmit () {
+      // 初步表單驗證
+      if(!this.name) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請輸入名稱'
+        })
+        return
+      } else if(!this.email) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請輸入Email'
+        })
+        return
+      } else if(!this.password || !this.passwordCheck) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請確實輸入二次密碼'
+        })
+        return
+      } else if(this.password !== this.passwordCheck) {
+        Toast.fire({
+          icon: 'warning',
+          title: '二次密碼輸入不一致，請重新輸入'
+        })
+        return
+      }
 
-      // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log('data', data)
+      try {
+        this.isProcessing = true
+        const { data } = await authorizationAPI.signUp({
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          passwordCheck: this.passwordCheck
+        })
+
+        if(data.status !== 'success') {
+          if (data.message === '信箱重複！') {
+            Toast.fire({
+              icon: 'error',
+              title: '此信箱已存在！請重新輸入信箱'
+            })
+            this.email = ''
+            this.isProcessing = false
+            return
+          }
+          throw new Error(data.message)
+        }
+        
+        this.$router.push({ name: 'sign-in' })
+        
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法註冊，請稍後再試'
+        })
+        console.log('error', error)
+      }
     }
   }
 }
